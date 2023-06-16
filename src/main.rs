d0be4cf452775e11dd51908668de43881eb3aa03
Daniel <ddeityy@gmail.com>
2023-06-17 00:30:21 +0300

    icewind

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use async_log_watcher::LogWatcherSignal;
-use rcon::{AsyncStdStream, Connection, Error};
-use toml;
+use rcon::{Connection, Error};
+use steamlocate::SteamDir;
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Start TF2 and sleep for 60s
-    std::process::Command::new("steam")
-        .arg("steam://rungameid/440")
-        .spawn()?;
-    let config: toml::Value = toml::from_str(include_str!("../config.toml")).unwrap();
-    let path = &config["server"]["tf2_path"]
-        .as_str()
-        .expect("Could not read path");
+    // Start TF2
+    //std::process::Command::new("steam")
+    //    .arg("steam://rungameid/440")
+    //    .spawn()?;
+
+    let mut path = SteamDir::locate().unwrap();
+    match path.app(&440) {
+        Some(_) => (),
+        None => panic!("Couldn't locate TF2 on this computer!"),
+    }
 
     // spawn() returns a task handle that must be awaited somewhere to run the log reading loop
-    let mut log_watcher = async_log_watcher::LogWatcher::new(path.to_string() + "/tf/console.log");
+    let mut log_watcher =
+        async_log_watcher::LogWatcher::new(path.path.join("tf").join("console.log"));
     let log_watcher_handle = log_watcher.spawn(false);
 
     // New task or thread or whatever. I'd prefer to not spawn tasks or include a tokio/rt dependency
@@ -25,37 +29,27 @@ async fn main() -> Result<(), Error> {
     });
 
     let mut connected: bool = false;
-    // log_watcher.try_read_message() for non blocking reading
+    let mut conn = create_connection().await;
     while let Some(data) = log_watcher.read_message().await {
         for line in std::str::from_utf8(&data).unwrap().split('\n') {
             if line.contains("CTFGCClientSystem::ShutdownGC") {
-                std::fs::File::create(path.to_string() + "/tf/console.log")?;
+                std::fs::File::create(path.path.join("tf").join("console.log"))?;
                 std::process::exit(1);
             }
-            //println!("{}", line);
             if line.contains("Connected to") {
                 println!("{}", line);
                 std::thread::sleep(std::time::Duration::from_secs(10));
                 connected = true;
             }
             if connected {
-                if line.contains("[SOAP] Soap DM unloaded.") {
-                    let mut conn = create_connection().await;
+                if line.contains("[SOAP] Soap DM unloaded.") | line.contains("[P-REC] Recording...")
+                {
                     demo(&mut conn, "ds_record").await?;
                 }
 
-                if line.contains("[demos.tf]: Demo recording completed") {
-                    let mut conn = create_connection().await;
-                    demo(&mut conn, "ds_stop").await?;
-                }
-
-                if line.contains("test record") {
-                    let mut conn = create_connection().await;
-                    demo(&mut conn, "ds_record").await?;
-                }
-
-                if line.contains("test stop") {
-                    let mut conn = create_connection().await;
+                if line.contains("[LogsTF] Uploading logs...")
+                    | line.contains("[P-REC] Stop record.")
+                {
                     demo(&mut conn, "ds_stop").await?;
                 }
             }
@@ -70,19 +64,14 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn create_connection() -> Connection<AsyncStdStream> {
-    let config: toml::Value = toml::from_str(include_str!("../config.toml")).unwrap();
-    let address: &str = config["server"]["ip"].as_str().expect("Could not read IP");
-    let password = config["server"]["password"]
-        .as_str()
-        .expect("Could not read password");
-    return Connection::<AsyncStdStream>::builder()
-        .connect(address, password)
+async fn create_connection() -> Connection<TcpStream> {
+    return Connection::<TcpStream>::builder()
+        .connect("127.0.0.1:27015", "prec")
         .await
         .expect("Can't connect");
 }
 
-async fn demo(conn: &mut Connection<AsyncStdStream>, cmd: &str) -> Result<(), Error> {
+async fn demo(conn: &mut Connection<TcpStream>, cmd: &str) -> Result<(), Error> {
     println!("request: {}", cmd);
     let resp = conn.cmd(cmd).await?;
     println!("response: {}", resp);
