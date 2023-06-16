5bc830d60152da8227313126d2183f5ad38721c5
Daniel <ddeityy@gmail.com>
2023-06-17 01:53:04 +0300

    general

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # Long awaited "prec" for linux users.
 
-### Credit for the idea of using a local RCON goes to [@Bash-09](https://github.com/Bash-09)
+### Credit for the idea of using a local RCON goes to [@Bash-09](https://github.com/Bash-09).
+### Thanks to [@icewind](https://github.com/icewind1991/) for help with refactoring.
 <br>
-
-# TODO
-- :green_check_square: Auto start TF2
-- :green_check_square: Clean up after exit
-- :white_large_square: Convert config.toml to an external file 
 
 # Usage
 
@@ -22,9 +18,6 @@ Add these to your launch options:
 
 ```-condebug -conclearlog -usercon```
 
-Run TF2, when you boot into the main menu - run ./rust-prec
-
-Once you see your console is being mirrored in the output - you're all set.
-
-When the server unloads SOAP DM it will start recording.
-When demos.tf finishes uploading it will stop recording.
+Run ```install.sh```
+Run ```sudo systemctl enable --now prec.service```
+Check with ```systemctl status prec.service```
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,47 @@
 use async_log_watcher::LogWatcherSignal;
 use rcon::{Connection, Error};
+use std::{fs::File, path::PathBuf};
 use steamlocate::SteamDir;
 use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Start TF2
-    //std::process::Command::new("steam")
-    //    .arg("steam://rungameid/440")
-    //    .spawn()?;
-
-    let mut path = SteamDir::locate().unwrap();
-    match path.app(&440) {
-        Some(_) => (),
+    let mut steam_dir = SteamDir::locate().unwrap();
+    let path: PathBuf;
+    match steam_dir.app(&440) {
+        Some(app) => path = app.path.join("tf").join("console.log"),
         None => panic!("Couldn't locate TF2 on this computer!"),
     }
 
-    // spawn() returns a task handle that must be awaited somewhere to run the log reading loop
-    let mut log_watcher =
-        async_log_watcher::LogWatcher::new(path.path.join("tf").join("console.log"));
+    let mut log_watcher = async_log_watcher::LogWatcher::new(&path);
     let log_watcher_handle = log_watcher.spawn(false);
 
-    // New task or thread or whatever. I'd prefer to not spawn tasks or include a tokio/rt dependency
-    // so the caller is responsable to drive the future somewhere.
     tokio::task::spawn(async {
-        // This is going to run the log reading loop.
         log_watcher_handle.await.unwrap_or_else(|_err| ());
     });
 
-    let mut connected: bool = false;
-    let mut conn = create_connection().await;
     while let Some(data) = log_watcher.read_message().await {
         for line in std::str::from_utf8(&data).unwrap().split('\n') {
-            if line.contains("CTFGCClientSystem::ShutdownGC") {
-                std::fs::File::create(path.path.join("tf").join("console.log"))?;
-                std::process::exit(1);
+            if line.contains("[SOAP] Soap DM unloaded.") | line.contains("[P-REC] Recording...") {
+                let mut conn = create_connection().await;
+                send(&mut conn, "ds_record").await?;
+                File::create(&path)?;
             }
-            if line.contains("Connected to") {
-                println!("{}", line);
-                std::thread::sleep(std::time::Duration::from_secs(10));
-                connected = true;
-            }
-            if connected {
-                if line.contains("[SOAP] Soap DM unloaded.") | line.contains("[P-REC] Recording...")
-                {
-                    demo(&mut conn, "ds_record").await?;
-                }
 
-                if line.contains("[LogsTF] Uploading logs...")
-                    | line.contains("[P-REC] Stop record.")
-                {
-                    demo(&mut conn, "ds_stop").await?;
-                }
+            if line.contains("[LogsTF] Uploading logs...") | line.contains("[P-REC] Stop record.") {
+                let mut conn = create_connection().await;
+                send(&mut conn, "ds_stop").await?;
+                File::create(&path)?;
+            }
+
+            if line.contains("test") {
+                let mut conn = create_connection().await;
+                send(&mut conn, "say test").await?;
+                File::create(&path)?;
             }
         }
     }
 
-    // Close log watcher by sending some command. You can also reload the file or change the file being read
     log_watcher
         .send_signal(LogWatcherSignal::Close)
         .await
@@ -71,9 +56,7 @@ async fn create_connection() -> Connection<TcpStream> {
         .expect("Can't connect");
 }
 
-async fn demo(conn: &mut Connection<TcpStream>, cmd: &str) -> Result<(), Error> {
-    println!("request: {}", cmd);
-    let resp = conn.cmd(cmd).await?;
-    println!("response: {}", resp);
+async fn send(conn: &mut Connection<TcpStream>, cmd: &str) -> Result<(), Error> {
+    conn.cmd(cmd).await?;
     Ok(())
 }
