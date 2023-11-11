ef130a5686dab06da613d177dc1992055c82d3e9
ddeity <ddeity@gmail.com>
2023-11-12 01:54:56 +0300

    updated script

--- install.sh
+++ install.sh
@@ -1,31 +1,22 @@
 #!/usr/bin/bash
-echo "Deleting previous install"
-sudo rm /home/$SUDO_USER/.local/bin/rust-prec
-sudo rm /etc/systemd/system/rust-prec.service
-sudo rm /usr/local/bin/prec.sh
-echo "Copying the executable and script"
-sudo cp target/release/rust-prec /home/$SUDO_USER/.local/bin/
+echo "Copying the executable"
+cp target/release/rust-prec /home/$SUDO_USER/.local/share/
 echo "Creating service file"
-sudo touch /etc/systemd/system/rust-prec.service
-sudo bash -c 'cat' << EOF > /etc/systemd/system/rust-prec.service
+sudo touch /etc/systemd/user/rust-prec.service
+sudo bash -c 'cat' << EOF > /etc/systemd/user/prec.service
 [Unit]
 Description=Rust prec service
 [Service]
-ExecStart=/usr/bin/bash /usr/local/bin/prec.sh
+ExecStart=/home/$SUDO_USER/.local/share/rust-prec
 Restart=on-failure
-User=$SUDO_USER
 [Install]
-WantedBy=multi-user.target
+WantedBy=default.target
 EOF
-echo "Creating systemd script"
-sudo touch prec.sh
-sudo bash -c 'cat' << EOF > prec.sh
-#!/usr/bin/bash
-/home/$SUDO_USER/.local/bin/rust-prec
-EOF
-sudo cp prec.sh /usr/local/bin/
 echo "Reloading systemd daemon"
-sudo systemctl daemon-reload
+systemctl --user -M $SUDO_USER@ daemon-reload
 echo "Enabling and starting the service"
-sudo systemctl enable --now rust-prec.service
-sudo systemctl status rust-prec.service
+echo ------------------------------------
+echo | cat /etc/systemd/user/prec.service
+echo ------------------------------------
+systemctl --user -M $SUDO_USER@ enable --now prec.service
+systemctl --user -M $SUDO_USER@ status prec.service
