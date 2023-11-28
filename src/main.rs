bb22875a742ae6639534b2a4c0607baff9dee1b4
ddeity <ddeity@gmail.com>
2023-11-29 01:22:47 +0300

    debounce

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use async_log_watcher::LogWatcher;
+use debounce::EventDebouncer;
+use futures_channel::mpsc::{Receiver, Sender};
 use log::info;
 use rcon::{Connection, Error};
 use std::env::var;
@@ -6,6 +8,13 @@ use std::fs::OpenOptions;
 use std::path::PathBuf;
 use steamlocate::SteamDir;
 use tokio::net::TcpStream;
+use tokio::time::Duration;
+
+#[derive(PartialEq)]
+enum ConsoleEvent {
+    Record,
+    Stop,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -26,22 +35,54 @@ async fn main() -> Result<(), Error> {
     let log_watcher_handle = log_watcher.spawn(true);
     tokio::task::spawn(log_watcher_handle);
 
+    let (sender, mut receiver): (Sender<ConsoleEvent>, Receiver<ConsoleEvent>) =
+        futures_channel::mpsc::channel(1024);
+
+    let delay = Duration::from_millis(100);
+    let debouncer = EventDebouncer::new(delay, move |event: ConsoleEvent| {
+        match_event(event, sender.clone())
+    });
+
+    tokio::spawn(async move {
+        while let Some(event) = receiver.try_next().unwrap_or_default() {
+            match event {
+                ConsoleEvent::Record => record(&rcon_password).await,
+                ConsoleEvent::Stop => stop(&rcon_password).await,
+            }
+        }
+    });
+
     while let Some(data) = log_watcher.read_message().await {
         for line in String::from_utf8(data).unwrap_or_default().split('\n') {
             if line.contains("[SOAP] Soap DM unloaded.") | line.contains("[P-REC] Recording...") {
-                let mut conn = create_connection(&rcon_password).await;
-                conn.cmd("ds_record").await?;
-                info!("Started recording");
+                debouncer.put(ConsoleEvent::Record);
             }
 
             if line.contains("[LogsTF] Uploading logs...") | line.contains("[P-REC] Stop record.") {
-                let mut conn = create_connection(&rcon_password).await;
-                conn.cmd("ds_stop").await?;
-                info!("Stopped recording");
+                debouncer.put(ConsoleEvent::Stop);
             }
         }
     }
     Ok(())
+}
+
+async fn record(rcon_password: &str) {
+    let mut conn = create_connection(&rcon_password).await;
+    conn.cmd("ds_record").await.unwrap();
+    info!("Started recording");
+}
+
+async fn stop(rcon_password: &str) {
+    let mut conn = create_connection(&rcon_password).await;
+    conn.cmd("ds_stop").await.unwrap();
+    info!("Stopped recording");
+}
+
+fn match_event(event: ConsoleEvent, mut sender: Sender<ConsoleEvent>) {
+    match event {
+        ConsoleEvent::Record => sender.try_send(event).unwrap_or_default(),
+        ConsoleEvent::Stop => sender.try_send(event).unwrap_or_default(),
+    }
 }
 
 fn log_path() -> Option<PathBuf> {
