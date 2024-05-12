8fcdba2ea3eaf61928999ff0ae9e04037970f006
ddeityy <ddeityy@gmail.com>
2024-05-12 18:47:45 +0300

    share > bin

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 echo "Deleting old binary"
-rm /home/$SUDO_USER/.local/share/rust-prec
+rm /home/$SUDO_USER/.local/bin/rust-prec
 echo "Copying binary"
-cp target/release/rust-prec /home/$SUDO_USER/.local/share/
+cp target/release/rust-prec /home/$SUDO_USER/.local/bin/
 echo "Creating service file"
-sudo touch /etc/systemd/user/rust-prec.service
+sudo touch /etc/systemd/user/prec.service
 sudo bash -c 'cat' << EOF > /etc/systemd/user/prec.service
 [Unit]
 Description=Rust prec service
 [Service]
-ExecStart=/home/$SUDO_USER/.local/share/rust-prec
+ExecStart=/home/$SUDO_USER/.local/bin/rust-prec
 Restart=on-failure
 [Install]
 WantedBy=default.target
