56bb97e4c48c4de1ff2e08fb44e641cb9cb69686
ddeityy <ddeityy@gmail.com>
2024-08-26 00:46:49 +0300

    Change screenshot to use F5, add clearing inputs to avoid ddddstatus etc.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,5 +1,5 @@
 use evdev::uinput::{VirtualDevice, VirtualDeviceBuilder};
-use evdev::{AttributeSet, Key, Synchronization};
+use evdev::{AttributeSet, Key};
 use evdev::{EventType, InputEvent};
 use std::thread::sleep;
 use std::time::Duration;
@@ -7,31 +7,48 @@ use std::time::Duration;
 use crate::EventHandler;
 
 fn press(dev: &mut VirtualDevice, key: Key) {
-    dev.emit(&[InputEvent::new(
-        EventType::SYNCHRONIZATION,
-        Synchronization::SYN_REPORT.0,
-        1,
-    )])
-    .unwrap();
-
     dev.emit(&[InputEvent::new(EventType::KEY, key.code(), 1)])
         .unwrap();
 
     sleep(Duration::from_millis(50));
+
     dev.emit(&[InputEvent::new(EventType::KEY, key.code(), 0)])
         .unwrap();
-
-    dev.emit(&[InputEvent::new(
-        EventType::SYNCHRONIZATION,
-        Synchronization::SYN_REPORT.0,
-        0,
-    )])
-    .unwrap();
 }
 
 impl<'a> EventHandler<'a> {
     pub fn take_screenshot(&mut self) {
-        press(&mut self.keyboard, Key::KEY_F12);
+        press(&mut self.keyboard, Key::KEY_F5);
+    }
+}
+
+impl<'a> EventHandler<'a> {
+    pub fn clear_input(&mut self) {
+        self.keyboard
+            .emit(&[InputEvent::new(EventType::KEY, Key::KEY_LEFTCTRL.code(), 1)])
+            .unwrap();
+        self.keyboard
+            .emit(&[InputEvent::new(EventType::KEY, Key::KEY_A.code(), 1)])
+            .unwrap();
+
+        sleep(Duration::from_millis(50));
+
+        self.keyboard
+            .emit(&[InputEvent::new(EventType::KEY, Key::KEY_A.code(), 0)])
+            .unwrap();
+        self.keyboard
+            .emit(&[InputEvent::new(EventType::KEY, Key::KEY_LEFTCTRL.code(), 0)])
+            .unwrap();
+
+        self.keyboard
+            .emit(&[InputEvent::new(EventType::KEY, Key::KEY_DELETE.code(), 1)])
+            .unwrap();
+
+        sleep(Duration::from_millis(50));
+
+        self.keyboard
+            .emit(&[InputEvent::new(EventType::KEY, Key::KEY_DELETE.code(), 0)])
+            .unwrap();
     }
 }
 
@@ -54,7 +71,10 @@ pub fn create_device() -> Result<VirtualDevice, std::io::Error> {
         Key::KEY_T,
         Key::KEY_U,
         Key::KEY_ENTER,
-        Key::KEY_F12,
+        Key::KEY_F5,
+        Key::KEY_DELETE,
+        Key::KEY_LEFTCTRL,
+        Key::KEY_BACKSPACE,
     ]
     .iter()
     .collect::<AttributeSet<_>>();
